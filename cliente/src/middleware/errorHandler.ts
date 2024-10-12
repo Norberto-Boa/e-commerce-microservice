13d454f0214ad088c507ab831e851b930f8241b7
Norberto Boa <nboa.26@gmail.com>
2024-10-12 14:23:59 +0200

    ADD: Create error handler

---
+++ cliente/src/middleware/errorHandler.ts
@@ -0,0 +1,12 @@
+import type { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (
+	error: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	return res.status(500).json({
+		message: error.message,
+	});
+};
--- cliente/src/server.ts
+++ cliente/src/server.ts
@@ -1,10 +1,13 @@
+import "express-async-errors";
 import express from "express";
 import { router } from "./infra/routes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
